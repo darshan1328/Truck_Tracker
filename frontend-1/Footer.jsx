c175965ed7c9a0da243ee3455731bdfdb0a0b414
Darshan M N <mn.darshan014@gmail.com>
2024-12-03 17:48:21 +0530

    ss

--- frontend-1/Footer.jsx
+++ frontend-1/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>© 2024 Truck Tracker. All rights reserved.</p>
+            <p>© 2024 Truck Tracker. All rights uy reserved.</p>
         </footer>
     );
 };
